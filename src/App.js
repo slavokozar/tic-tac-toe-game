e646425270bb67359f21479157e515b985c5c87e
Slavo Kozar <slavo.kozar@gmail.com>
2019-11-08 09:53:22 +0100

    initial commit - rendering grid

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+    constructor( props ) {
+        super( props );
 
-export default App;
+        this.state = {
+            grid: [
+                [null, null, null],
+                [null, null, null],
+                [null, null, null]
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div style={{width: '100vw', height: '100vh', display: 'flex', background: '#aaa'}}>
+                <div style={{width: '60vw', height: '40vw', margin: 'auto'}}>
+                    {
+                        this.state.grid.map( ( row ) => (
+                            <div style={{display: 'flex', flexDirection: 'row'}}>
+                                {
+                                    row.map( ( symbol ) => (
+                                        <div style={{
+                                            width:      '10vw',
+                                            height:     '10vw',
+                                            fontSize:   '3rem',
+                                            margin:     '3px',
+                                            background: '#fff'
+                                        }}>{ symbol }</div>
+                                    ) )
+                                }
+                            </div>
+                        ) )
+                    }
+                </div>
+            </div>
+        )
+    }
+
+}
