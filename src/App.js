4ffdd6093920f4200db3b250deb5f25ddf4c4398
Slavo Kozar <slavo.kozar@gmail.com>
2020-03-19 10:28:38 +0100

    add comments

--- src/App.js
+++ src/App.js
@@ -40,17 +40,27 @@ export default class App extends React.Component {
                                                 //onClick={this.handleClick}
 
                                                  onClick={() => {
+
+                                                     // because of arrow function we can use the variables i and j
+                                                     // those variables came from two map methods our code is wrapped into
+                                                     console.log('clicked', i, j);
+
+                                                     // create a copy of state variables we can modify
+                                                     // remember we can't modify state directly!
                                                      let newGrid = this.state.grid;
                                                      let newPlayer = this.state.player;
 
                                                      if ( this.state.player === 1 ) {
+                                                         // put the symbol x on coordinates [i, j] into the grid
                                                          newGrid[ i ][ j ] = 'x';
+                                                         // change player to player 2
                                                          newPlayer = 2;
                                                      } else {
                                                          newGrid[ i ][ j ] = 'o';
                                                          newPlayer = 1;
                                                      }
 
+                                                     // store new values to state of component
                                                      this.setState( {
                                                          grid: newGrid,
                                                          player: newPlayer
