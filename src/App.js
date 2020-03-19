261c629336ab674ce0384884e5968cdfa509f203
Slavo Kozar <slavo.kozar@gmail.com>
2020-03-19 09:27:34 +0100

    grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 export default class App extends React.Component {
@@ -8,23 +7,18 @@ export default class App extends React.Component {
 
         this.state = {
             grid:   [
-                [null, null, null],
-                [null, null, null],
-                [null, null, null]
+                ['', '', '', ''],
+                ['', '', '', ''],
+                ['', '', '', ''],
+                ['', '', '', ''],
             ],
             player: 1
         }
     }
 
-    handleClick() {
-        console.log( 'click' );
-
-    }
-
     render() {
         return (
             <>
-                <h1>Player {this.state.player} is playing</h1>
                 <div style={{width: '100vw', height: '100vh', display: 'flex', background: '#aaa'}}>
                     <div style={{width: '60vw', height: '40vw', margin: 'auto'}}>
                         {
@@ -38,31 +32,9 @@ export default class App extends React.Component {
                                                 fontSize:   '3rem',
                                                 margin:     '3px',
                                                 background: '#fff'
-                                            }}
-                                                 onClick={() => {
-                                                     console.log( 'clicked', i, j );
-
-                                                     let grid = this.state.grid;
-
-                                                     // if(grid[ i ][ j ]) return;
-                                                     if(grid[ i ][ j ] !== null) return;
-
-                                                     grid[ i ][ j ] = this.state.player === 1 ? 'X' : 'O';
-
-                                                     // if(this.state.player === 1){
-                                                     //     this.setState({player:2})
-                                                     // }else{
-                                                     //     this.setState({player:1})
-                                                     // }
-
-                                                     this.setState( {
-                                                         grid: grid,
-                                                         player: 3 - this.state.player
-                                                     } );
-
-                                                 }}
-
-                                            >{symbol}</div>
+                                            }}>
+                                                {symbol}
+                                            </div>
                                         ) )
                                     }
                                 </div>
