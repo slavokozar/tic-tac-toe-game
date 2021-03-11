a9364dcc1c60d37cb24807e02c74fae92cf70f13
Slavo Kozar <slavo.kozar@gmail.com>
2021-03-11 22:27:43 +0100

    cool design

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ export default function App() {
 
                                          }}
                                     >
-                                        {cell}
+                                        <span>{cell}</span>
                                     </div>
 
                                 ) )
